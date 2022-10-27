25e24cfe6c6580072fddb686a11b49eb0f3ea6c8
shubbamshrmali <shubbamshrmali@gmail.com>
2022-10-27 17:58:09 -0500

    updated with jenkins

--- part1/main.tf
+++ part1/main.tf
@@ -41,3 +41,17 @@ resource "aws_instance" "docker" {
     user_data = file("docker.sh")
 
 }
+
+resource "aws_instance" "jenkins" {
+    ami = var.ec2_ami
+    count = var.ec2_count
+    key_name   = var.key_name
+    instance_type = var.instance_type
+    security_groups = ["${var.ec2_sg}"]
+    subnet_id = element(var.ec2_subnet_id, count.index) #element(list, index)
+    tags = {
+        Name = "${var.ec2_tags[2]}"
+    } 
+    user_data = file("jenkinsServer.sh")
+
+}
--- part1/variables.tf
+++ part1/variables.tf
@@ -37,7 +37,7 @@ variable "ec2_subnet_id" {
 }
 
 variable "ec2_tags" {
-    default = ["Tomcat","Docker"]
+    default = ["Tomcat","Docker","Jenkins"]
   
 }
 
